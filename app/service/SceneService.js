8e057aba35c5a2c39e815971f1f0ab9073769915
Daniele Callegaro <daniele.callegaro.90@gmail.com>
2015-01-22 17:47:00 +0100

    Aggiunto il reycaster nel SceneService

--- app/service/SceneService.js
+++ app/service/SceneService.js
@@ -2,7 +2,7 @@
 
 angular.module('myApp')
 	.service('SceneService', function(CameraService, $rootScope) {
-		var scene,domElement,renderer;
+		var scene,domElement,renderer,raycaster,mouse;
 		var lights = new Array();
 		this.setScene = function(_scene) { scene = _scene; }
 		this.getScene = function() { return scene; }
@@ -15,11 +15,23 @@ angular.module('myApp')
 		{
 			requestAnimationFrame(this.render.bind(this));
 			renderer.render(scene, CameraService.getCamera());
+			
 			TWEEN.update();
 		}
 		this.onResize = function(width,height)
 		{
 			renderer.setSize( width, height );
 		}
+		var init = function()
+		{
+			raycaster = new THREE.Raycaster();
+			mouse = new THREE.Vector2();
+		}
+		this.onMouseMove = function( event ) {
+			mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+			mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+			raycaster.setFromCamera( mouse, CameraService.getCamera());
+		}
+		init();
 	}
 );
